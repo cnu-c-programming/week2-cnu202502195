c5b41b947ec38a78e0fb794386d31e05477c5054
cnu202502195 <202502195@o.cnu.ac.kr>
2026-03-13 09:36:31 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -14,8 +14,8 @@ int main()
     printf("string : %s\n", var3);
 
     float var4 = 3.14592;
-    printf("float : %f\n, var4);
-    printf("exp : %e\n, var4);
+    printf("float : %f\n", var4);
+    printf("exp : %e\n", var4);
 
     return 0;
 }
