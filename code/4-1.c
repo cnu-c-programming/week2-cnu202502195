2267e770b2c790f4a30b3647c14b3a575e16ba19
cnu202502195 <202502195@o.cnu.ac.kr>
2026-03-16 22:24:35 +0900

    4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int a, b;
+    char c;
+
+    scanf("%d %d %c", &a, &b, &c);
+
+    if(c == '+') printf("%d\n", a+b);
+    else if(c == '-') printf("%d\n", a-b);
+    else if(c == '*') printf("%d\n", a*b);
+    else if(c == '/') printf("%d\n", a/b);
 
     return 0;
-}
 
+}
