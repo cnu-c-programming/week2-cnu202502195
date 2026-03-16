4038f09fbd9f44ef1e7ee524b86502e1723c288e
cnu202502195 <202502195@o.cnu.ac.kr>
2026-03-16 23:45:25 +0900

    4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int a, i;
+    int prime;
 
+    scnaf("%d", &a);
+
+    if (a < 2) {
+        prime = 0;
+    }
+    for(i = 2; i < a; i++) {
+        if (a % i == 0) {
+            prime = 0;
+            break;
+        
+        }
+    }
+    if(prime) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
     return 0;
 }
-
